eeea323fc139e8398b0673b04100ad06e991715a
fshang3 <FengShang@users.noreply.github.com>
2018-08-10 13:49:49 -0500

    finish import option from json file

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,7 +6,7 @@
     <select id="yearId" class="smSelectBox">
       <option class="display-none">Year</option>
     </select>
-    <form_select :selectfrom="Make" @change="makechange='true'">Select Make</form_select>
+    <form_select_new :option="Make"></form_select_new>
     <form_select :selectfrom="Model" :disabled="makechange=='false'" @change="modelchange='true'">Select Model</form_select>
     <form_select :selectfrom="Trim" :disabled="modelchange=='false'">Select Trim</form_select>
     <input type="text" id="zipcode" class="smSelectBox" placeholder="ZIP" maxlength="5" pattern="\d*" title="zipcode" />
@@ -16,29 +16,23 @@
   </div>
 </template>
 <script>
+  import Option from "../../static/option.json"
   import form_select from './Form_select.vue'
+  import form_select_new from './Form_select_new.vue'
   import form_submit from './Form_submit.vue'
   export default {
     name: 'input_form',
     components: {
       form_select,
       form_submit,
+      form_select_new
     },
     data: function () {
 
       return {
+        Make: Option.Make,
         makechange: 'false',
         modelchange:'false',
-        Make: [
-          { text: 'Make1', value: '1' },
-          { text: 'Make2', value: '2' }
-        ],
-        Model: [
-          { text: 'Model1', value: '1' },
-          { text: 'Model2', value: '2' }],
-        Trim: [
-          { text: 'Trim1', value: '1' },
-          { text: 'Trim2', value: '2' }],
       }
     }
   }
