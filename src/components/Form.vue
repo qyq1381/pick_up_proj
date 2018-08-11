b6c1cba5c870438536ae916642e69242a90b26d9
fshang3 <39886473+fshang3@users.noreply.github.com>
2018-08-11 18:05:36 -0500

    Fix Year

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <h1>First, tell us about your junker.</h1>
+    <h4>Then, make cash in seconds.</h4>
+    <select v-model="selectedYear">
+      <option hidden disabled :value="selectedYear" v-if="selectedYear==''">Select Year</option>
+      <option v-for="year in YearArrary()" value="year">{{year}}</option>
+    </select>
     <select v-model="selectedDrink" @change="selectDrink">
       <option hidden disabled value="-1">SelectDrink</option>
       <option v-for="(drink,index) in drinks" :value="index">{{ drink.label }}</option>
     </select>
 
-    <select v-model="selectedOption" >
-      <option disabled :value="selectedOption" v-if="selectedOption==''">SelectSubDrink</option>
+    <select v-model="selectedOption">
+      <option hidden disabled :value="selectedOption" v-if="selectedOption==''">SelectSubDrink</option>
       <option v-for="option in options">{{ option }}</option>
     </select>
 
@@ -21,7 +27,7 @@
     components: {
     },
     data: function () {
-
+      
       return {
         drinks: [
           {
@@ -41,7 +47,8 @@
         selectedDrink: -1,
         selectedOption: '',
         options: [],
-        selectedDrinkLabel: ''
+        selectedDrinkLabel: '',
+        selectedYear:'',
       }
     },
     methods: {
@@ -52,7 +59,18 @@
       },
       getOption: function () {
         return this.selectedOption;
+      },
+      
+      YearArrary: function () {
+        let Year_Arrary = [];
+        let endYear = new Date().getFullYear() + 1;
+        while (endYear >= 1950) {
+          Year_Arrary.push(endYear);
+          endYear--;
+        }
+        return Year_Arrary
       }
+
     }
   }
 </script>
