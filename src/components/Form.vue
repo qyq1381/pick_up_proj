58d3f04010f4bb4c9808d19ff936a58a5a45cb6b
fshang3 <FengShang@users.noreply.github.com>
2018-08-11 00:14:11 -0500

    1 file form! no sub component!!!!!

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,38 +1,57 @@
 <template>
   <div>
-    <h1>First, tell us about your junker.</h1>
-    <h4>Then, make cash in seconds.</h4>
-
-    <select id="yearId" class="smSelectBox">
-      <option class="display-none">Year</option>
+    <select v-model="selectedDrink" @change="selectDrink">
+      <option hidden disabled value="-1">SelectDrink</option>
+      <option v-for="(drink,index) in drinks" :value="index">{{ drink.label }}</option>
     </select>
-    <form_select_new :option="Make"></form_select_new>
-    <form_select :selectfrom="Model" :disabled="makechange=='false'" @change="modelchange='true'">Select Model</form_select>
-    <form_select :selectfrom="Trim" :disabled="modelchange=='false'">Select Trim</form_select>
-    <input type="text" id="zipcode" class="smSelectBox" placeholder="ZIP" maxlength="5" pattern="\d*" title="zipcode" />
 
-    <button id="submitbutton" type="button" class="btn btn-warning btn-lg">Continue</button>
+    <select v-model="selectedOption" >
+      <option disabled :value="selectedOption" v-if="selectedOption==''">SelectSubDrink</option>
+      <option v-for="option in options">{{ option }}</option>
+    </select>
 
+    <p v-if="selectedOption">
+      You selected a {{ selectedDrinkLabel }} and specifically {{ selectedOption }}.
+    </p>
   </div>
 </template>
 <script>
-  import Option from "../../static/option.json"
-  import form_select from './Form_select.vue'
-  import form_select_new from './Form_select_new.vue'
-  import form_submit from './Form_submit.vue'
   export default {
     name: 'input_form',
     components: {
-      form_select,
-      form_submit,
-      form_select_new
     },
     data: function () {
 
       return {
-        Make: Option.Make,
-        makechange: 'false',
-        modelchange:'false',
+        drinks: [
+          {
+            label: "Beer",
+            options: ["Sam Adams", "Anchor Steam", "St. Arnold"]
+          },
+          {
+            label: "Soda",
+            options: ["Pepsi", "Coke", "RC"]
+          },
+          {
+            label: "Coffee",
+            options: ["Starbucks", "Dunkin Donuts", "Gross Hotel Room"]
+          }
+        ],
+
+        selectedDrink: -1,
+        selectedOption: '',
+        options: [],
+        selectedDrinkLabel: ''
+      }
+    },
+    methods: {
+      selectDrink: function () {
+        this.selectedOption = '';
+        this.options = this.drinks[this.selectedDrink].options;
+        this.selectedDrinkLabel = this.drinks[this.selectedDrink].label;
+      },
+      getOption: function () {
+        return this.selectedOption;
       }
     }
   }
