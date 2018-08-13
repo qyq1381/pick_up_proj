2ad125d396470e689460925d30a4c8030e0031ce
fshang3 <39886473+fshang3@users.noreply.github.com>
2018-08-12 19:38:44 -0500

    Finish Button

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Hind', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>
+  <div id="form">
     <h1>First, tell us about your junker.</h1>
     <h4>Then, make cash in seconds.</h4>
-    <select v-model="selectedYear">
-      <option hidden disabled :value="selectedYear" v-if="selectedYear==''">Select Year</option>
-      <option v-for="year in YearArrary()" value="year">{{year}}</option>
-    </select>
-    <select v-model="selectedDrink" @change="selectDrink">
-      <option hidden disabled value="-1">SelectDrink</option>
-      <option v-for="(drink,index) in drinks" :value="index">{{ drink.label }}</option>
-    </select>
+    <div class="selectcontainer">
+      <select v-model="selectedYear" class="smselectbox">
+        <option hidden disabled :value="selectedYear" v-if="selectedYear==''">Select Year</option>
+        <option v-for="year in YearArrary()" :value="year">{{year}}</option>
+      </select>
+      <select v-model="selectedDrink" @change="selectDrink" class="lgselectbox">
+        <option hidden disabled :value=selectedDrink v-if="selectedDrink==''">SelectDrink</option>
+        <option v-for="(drink,index) in drinks" :value="index">{{ drink.label }}</option>
+      </select>
 
-    <select v-model="selectedOption">
-      <option hidden disabled :value="selectedOption" v-if="selectedOption==''">SelectSubDrink</option>
-      <option v-for="option in options">{{ option }}</option>
-    </select>
-
-    <p v-if="selectedOption">
-      You selected a {{ selectedDrinkLabel }} and specifically {{ selectedOption }}.
-    </p>
+      <select v-model="selectedOption" class="lgselectbox">
+        <option hidden disabled :value="selectedOption" v-if="selectedOption==''">SelectSubDrink</option>
+        <option v-for="option in options">{{ option }}</option>
+      </select>
+    </div>
+    <button id="submitbutton" @click="submitvalue(selectedYear,selectedDrink,selectedOption)">Continue</button>
   </div>
 </template>
 <script>
@@ -27,7 +26,7 @@
     components: {
     },
     data: function () {
-      
+
       return {
         drinks: [
           {
@@ -44,11 +43,11 @@
           }
         ],
 
-        selectedDrink: -1,
+        selectedDrink: '',
         selectedOption: '',
         options: [],
         selectedDrinkLabel: '',
-        selectedYear:'',
+        selectedYear: '',
       }
     },
     methods: {
@@ -60,7 +59,7 @@
       getOption: function () {
         return this.selectedOption;
       },
-      
+
       YearArrary: function () {
         let Year_Arrary = [];
         let endYear = new Date().getFullYear() + 1;
@@ -69,10 +68,83 @@
           endYear--;
         }
         return Year_Arrary
-      }
+      },
+      submitvalue: function (year_v, drink_v, option_v) {
+        let data = {
+          "year": year_v,
+          "drink": this.drinks[drink_v].label,
+          "option": option_v,
+        }
 
+        this.$http.post('https://postman-echo.com/get?foo1=bar1&foo2=bar2', data);wqwwda
+        alert(data)
+      },
+      
     }
   }
 </script>
-<style>
+<style scoped>
+
+  h1 {
+    padding: 100px 0 0 0;
+    font-size: 2.5rem;
+  }
+
+  h4 {
+    padding: 30px 0 0 0;
+    font-size: 1.5rem;
+  }
+
+  h1, h4 {
+    font-family: inherit;
+    font-weight: 500;
+    line-height: 1.1;
+    color: inherit;
+  }
+
+  select {
+    font-size: 1rem;
+  }
+
+  .smselectbox {
+    display: inline-block;
+    padding: 15px 20px;
+    margin: 0 3px;
+    width: 8%;
+  }
+
+  .lgselectbox {
+    display: inline-block;
+    padding: 15px 20px;
+    margin: 0 3px;
+    width: 15%;
+  }
+
+  button {
+    margin: 0 0 40px 0;
+    padding: .75rem 1.5rem;
+    font-size: 1.25rem;
+    border-radius: .3rem;
+    color: #fff;
+    background-color: #f0ad4e;
+    border-color: #f0ad4e;
+    font-weight: 400;
+    line-height: 1.25;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+  }
+
+  .selectcontainer {
+    padding: 40px 0 40px 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  #form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
