3cf2cd197956f2c16d9887184fb23eeffad4b07a
Lihong Han <littlefly14@gmail.com>
2018-08-13 19:10:37 -0500

    footer by vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,28 @@
   <div id="app">
     <navbar></navbar>
     <input_form></input_form>
+    <process_main title="There is nothing to it."></process_main>
+    <section>
+      <table_main></table_main>
+    </section>
+    <footer_main></footer_main>
   </div>
 </template>
 
 <script>
   import navbar from './components/NavBar.vue'
   import input_form from './components/Form.vue'
+  import process_main from './components/Process_main.vue'
+  import table_main from './components/table.vue'
+  import footer_main from './components/footer.vue'
   export default {
     name: 'App',
     components: {
       navbar,
       input_form,
+      process_main,
+      table_main,
+      footer_main
     }
 
   }
@@ -25,5 +36,8 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+  }
+  section {
+    padding: 50px;
   }
 </style>
