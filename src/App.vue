1b40a4f8806fcf01b3c7d53565407dac52a1c382
Lihong Han <littlefly14@gmail.com>
2018-08-20 15:26:11 -0500

    a

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,22 @@
 <template>
   <div id="app">
     <navbar></navbar>
-    <input_form></input_form>
-    <table_main></table_main>
+    <router-view style="flex-grow:1"></router-view>
     <footer_main></footer_main>
   </div>
 </template>
 
 <script>
-  import navbar from './components/NavBar.vue'
-  import input_form from './components/Form.vue'
-  import table_main from './components/table.vue'
-  import footer_main from './components/footer.vue'
-  export default {
-    name: 'App',
-    components: {
-      navbar,
-      input_form,
-      table_main,
-      footer_main
-    }
-}
+import navbar from "./components/NavBar.vue";
+import footer_main from "./components/footer.vue";
+
 export default {
   name: "App",
   components: {
     navbar,
     footer_main
   }
-}
+};
 </script>
 
 <style>
